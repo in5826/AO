e0dc131441040b41c40c83fa1a938818d5837be3
in5826 <116056215+in5826@users.noreply.github.com>
2025-08-06 19:56:18 +0900

    Add files via upload

---
+++ js/design-notes.js
@@ -0,0 +1,14 @@
+  const imagesToPreload = [
+    '../images//design-notes__item_l-4.png',
+    '../images/design-notes__item_l-5.png',
+    '../images/design-notes__item_l-6png.png',
+    '../images/design-notes__item_r-4.png',
+    '../images/design-notes__item_r-5png.png',
+    '../images/design-notes__item_r-6png.png'
+    // 여기에 필요한 이미지들 계속 추가!
+  ];
+
+  imagesToPreload.forEach((src) => {
+    const img = new Image();
+    img.src = src;
+  });
